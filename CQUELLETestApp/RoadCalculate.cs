ba8bce2b1a81027e19d5c293d66d1ffab98ce336
pflofif <bogdanvivchar8@gmail.com>
2023-03-13 22:32:22 +0200

    Start writting Arrival Time Calculator

---
+++ CQUELLETestApp/RoadCalculate.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Concurrent;
+using System.Linq.Expressions;
+
+namespace CQUELLETestApp;
+
+public class RoadCalculate
+{
+    private static readonly Range TimeOfWorkDay = 8..17;
+    private readonly TimeSpan _endOfWorkDay = TimeSpan.FromHours(TimeOfWorkDay.End.Value);
+    private readonly TimeSpan _startOfWorkDay = TimeSpan.FromHours(TimeOfWorkDay.Start.Value);
+    
+}
