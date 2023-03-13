bed46b33af0b12cea0965d03ae15606e6f3d86b7
pflofif <bogdanvivchar8@gmail.com>
2023-03-13 22:29:13 +0200

    using pattern Humble Object create IOSystemProvider.cs to incapsulate code from system, to better unit testing

---
+++ CQUELLETestApp/ConsoleProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace CQUELLETestApp;
+
+public class ConsoleProvider : IOSystemProvider
+{
+    public string ReadLine() => Console.ReadLine() ?? "";
+    public void WriteLine(object value) => Console.WriteLine(value);
+}
