50bdaa2f92515b2979046c5148b74300563aebcb
pflofif <bogdanvivchar8@gmail.com>
2023-03-13 23:08:52 +0200

    Write logic in RoadCalculate.cs and in Program.cs

--- CQUELLETestApp/Program.cs
+++ CQUELLETestApp/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using CQUELLETestApp;
+
+var provider = new DateService("dd.MM.yyyy HH:mm", new ConsoleProvider());
+var date = provider.InputAndGetDateTimeInFormat();
+
+var calculator = new RoadCalculate();
+var timeOfArrival = calculator.CalculateTime(date);
+
+Console.WriteLine(
+    $"The estimated delivery date and time is: {timeOfArrival:dd.MM.yyyy HH:mm}");
+
+Console.ReadKey();
